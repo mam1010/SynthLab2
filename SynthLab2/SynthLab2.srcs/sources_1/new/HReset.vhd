d60b59b9db421fc0f0ec74bbf2b6bd5d9f664a72
mam1010 <38589307+mam1010@users.noreply.github.com>
2019-02-11 15:16:12 -0500

    Add files via upload

---
+++ SynthLab2/SynthLab2.srcs/sources_1/new/HReset.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/11/2019 03:10:00 PM
+-- Design Name: 
+-- Module Name: HReset - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity HReset is
+    Port ( CLK : in STD_LOGIC;
+           RESET : in STD_LOGIC;
+           D : in STD_LOGIC;
+           Q : out STD_LOGIC);
+end HReset;
+
+architecture Behavioral of HReset is
+
+begin
+    process (CLK)
+begin
+   if CLK'event and CLK='1' then
+      if RESET='1' then
+         Q <= '0';
+      else
+         Q <= D;
+      end if;
+   end if;
+end process;
+
+end Behavioral;
